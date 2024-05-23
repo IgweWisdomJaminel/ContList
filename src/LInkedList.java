1f8f1811d1fa94400f579b3d8c34d5ea645f66c2
jaminel <jamineltechnology@gmail.com>
2024-05-24 00:00:09 +0100

    update

--- src/LInkedList.java
+++ src/LInkedList.java
@@ -1,18 +1,69 @@
 public class LInkedList  {
 
-    Node node;
+    Node head;
     int count;
 
-    public LInkedList(Node node) {
-        this.node = node;
+//    public LInkedList(){
+//        head=null;
+//        count=0;
+//    }
+
+    public LInkedList(Node newHead) {
+        this.head = newHead;
       count++;
     }
 
-    public Node getNode() {
-        return node;
+    public void add(int newData){
+        Node temp = new Node(newData);
+        Node current =head;
+        while (current.getNext()!=null){
+            current=current.getNext();
+        }
+        current.setNext(temp);
+        count++;
     }
 
-    public int getCount() {
-        return count;
+    public int getIndex(int index){
+        Node current=head;
+     for(int i=0; i<index; i++){
+         current= current.getNext();
+     }
+       return current.getData();
     }
+
+
+
+   public int getSize(){
+        return count;
+   }
+
+   public void addValue(int newData){
+        Node current = new Node(newData);
+        current= head;
+        if(current.getNext()!=null){
+            current=current.getNext();
+        }
+        current.setNext(current);
+   }
+
+   public boolean isEmpty(){
+        return head==null;
+   }
+
+   public void remove(){
+        Node current = head;
+
+        if(current.getNext()==null){
+            current=null;
+        }else {
+            System.out.println("Node is empty");
+        }
+
+       while(current.getNext().getNext()!=null){
+           current=current.getNext();
+       }
+       current.setNext(null);
+        count--;
+
+   }
 }
