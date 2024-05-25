154d024e769400cb2b6a99cebcfad9e7ad719bb6
jaminel <jamineltechnology@gmail.com>
2024-05-25 18:32:24 +0100

    update

---
+++ src/MyNode.java
@@ -0,0 +1,18 @@
+public class MyNode {
+    MyNode Next;
+    int data;
+
+    public MyNode(MyNode node, int data) {
+        this.data = data;
+        Next = null;
+
+    }
+
+    public void setNode(MyNode Next) {
+        Next = Next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
