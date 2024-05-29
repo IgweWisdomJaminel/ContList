4e6b7d7d3d8324984f6b7e6f20ead3a19701483c
jaminel <jamineltechnology@gmail.com>
2024-05-29 04:27:08 +0100

    new message

--- src/MysingleLinkedList.java
+++ src/MysingleLinkedList.java
@@ -1,9 +1,22 @@
 public class MysingleLinkedList {
 
-    MyNode head;
+    Node head;
 
     int count;
+
+
+    public void add(int data){
+
+        Node node= new Node(data);
+        Node current= head;
+
+        while(current.getNext()!=null){
+            current=current.getNext();
+        }
+        current.setNext(node);
+    }
     public static void main(String[] args) {
+
 
     }
 }
